d63b27cfe8e41161e9748d6ce80e113e9fc5a24a
Brady Mavetz <b.mavetz@outlook.com>
2022-04-18 22:28:41 -0500

    added files to store employee data, modified index.js to capture manager info

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateHTML = require('./src/generateHTML');
+const Team = [];
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+
+
+function getManager(){
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter the name of the manager',
+        },
+        {
+            type: 'input',
+            name: 'emplID',
+            message: 'Please enter employee ID number:'
+
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter the email address of the employee',
+        },
+        {
+            type: 'input',
+            name: 'officeNum',
+            message: 'Please enter the office number for the employee:',
+        }
+    ])
+    .then((answers) =>{
+        console.log(answers);
+        const managerInfo = new manager(answers.name, answers.emplID, answers.email, answers.officeNum);
+        team.push(managerInfo);
+    })
+}
